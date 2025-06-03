c203f2170e7be084e7da5522ea50b0b1d8864769
nataliafirmino <nataliaffirmino2401@gmail.com>
2025-06-03 20:53:37 -0300

    Comentário da aula 03/06

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require("express-session");
-require('dotenv/config');
+require('dotenv/config'); //aula dia 03/06
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
